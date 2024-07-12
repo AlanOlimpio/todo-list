dcb4fcd446032c89071554f9e609b31de413959e
Alan Olimpio <alancleyton.olimpio@gmail.com>
2024-07-12 10:12:34 -0300

    feat: adiciona lógica

--- src/Form/Form.tsx
+++ src/Form/Form.tsx
@@ -1,14 +1,42 @@
 import { PlusCircle } from 'phosphor-react';
 import Styled from './Form.module.css';
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from 'react';
 
 function Form() {
+  const [newTaskTex, setNewTaskTex] = useState('');
+  function handleCreateNewTask(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const eventTeste = new CustomEvent('newTask', {
+      bubbles: true,
+      cancelable: true,
+      detail: {
+        label: newTaskTex,
+      },
+    });
+    window.dispatchEvent(eventTeste);
+    setNewTaskTex('');
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('');
+    setNewTaskTex(event.target.value);
+  }
+
+  function handleNewTaskInvalid(event: FormEvent<HTMLInputElement>) {
+    const eventTarget = event as InvalidEvent<HTMLInputElement>;
+    eventTarget.target.setCustomValidity('Esse campo é obrigatório!');
+  }
+
   return (
-    <form action="" className={Styled.toDoForm}>
+    <form onSubmit={(e) => handleCreateNewTask(e)} className={Styled.toDoForm}>
       <input
-        name="create-todo"
+        name="create-task"
+        value={newTaskTex}
         placeholder="Adicione uma nova tarefa"
         type="text"
-        onChange={() => {}}
+        onChange={(e) => handleNewTaskChange(e)}
+        onInvalid={(e) => handleNewTaskInvalid(e)}
+        required
       />
       <button type="submit">
         Criar <PlusCircle size={16} weight="bold" />
--- src/List/List.tsx
+++ src/List/List.tsx
@@ -1,68 +1,111 @@
 import { Trash } from 'phosphor-react';
 import Styled from './List.module.css';
+import { MouseEvent, useEffect, useState } from 'react';
+interface TasksProps {
+  id: string;
+  label: string;
+  isChecked: boolean;
+}
+
 function List() {
+  const [tasks, setTask] = useState<TasksProps[]>([]);
+  const [checked, setChecked] = useState(0);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  function handleAddTask(event: CustomEventInit) {
+    const taskTemp = tasks[tasks.length - 1];
+    const newtTaskId = taskTemp?.id ? parseInt(taskTemp.id) + 1 : 1;
+
+    setTask([
+      ...tasks,
+      {
+        id: `${newtTaskId}`,
+        label: event.detail.label,
+        isChecked: false,
+      },
+    ]);
+  }
+
+  function handleConuntTask(tasksChecked: TasksProps[]) {
+    let checkeds = 0;
+    tasksChecked.filter((task) => {
+      if (task.isChecked) {
+        checkeds += 1;
+      }
+      return checkeds;
+    });
+    setChecked(checkeds);
+  }
+
+  function handleDeleteTask(event: MouseEvent<HTMLButtonElement>, id: string) {
+    event.stopPropagation();
+    const taskUpdate = tasks.filter((task) => task.id !== id);
+    setTask(taskUpdate);
+    handleConuntTask(taskUpdate);
+  }
+
+  function handleCheckedTask(id: string) {
+    const taskUpdate = tasks.map((task) => {
+      if (task.id === id) {
+        task.isChecked = !task.isChecked;
+      }
+      return { ...task };
+    });
+    setTask(taskUpdate);
+    handleConuntTask(taskUpdate);
+  }
+
+  useEffect(() => {
+    window.addEventListener('newTask', handleAddTask);
+    return () => window.removeEventListener('newTask', handleAddTask);
+  }, [handleAddTask]);
+
   return (
     <div className={Styled.content}>
       <div className={Styled.infoList}>
         <h2>
-          Tarefas criadas <span className={Styled.counter}>5</span>
+          Tarefas criadas <span className={Styled.counter}>{tasks.length}</span>
         </h2>
         <h2>
-          Concluídas <span className={Styled.counter}>2 de 5</span>
+          Concluídas{' '}
+          <span className={Styled.counter}>
+            {checked} de {tasks.length}
+          </span>
         </h2>
       </div>
-      <ul>
-        <li>
-          <input id="item-1" type="checkbox" name="item-1" value="item-1" />
-          <label htmlFor="item-1">
-            Integer urna interdum massa libero auctor neque turpis turpis
-            semper. Duis vel sed fames integer.
-          </label>
-          <button onClick={() => {}} title="Deletar tarefa">
-            <Trash size={18} />
-          </button>
-        </li>
-        <li>
-          <input id="item-2" type="checkbox" name="item-2" value="item-2" />
-          <label htmlFor="item-2">
-            Integer urna interdum massa libero auctor neque turpis turpis
-            semper. Duis vel sed fames integer.
-          </label>
-          <button onClick={() => {}} title="Deletar tarefa">
-            <Trash size={18} />
-          </button>
-        </li>
-        <li>
-          <input id="item-3" type="checkbox" name="item-3" value="item-3" />
-          <label htmlFor="item-3">
-            Integer urna interdum massa libero auctor neque turpis turpis
-            semper. Duis vel sed fames integer.
-          </label>
-          <button onClick={() => {}} title="Deletar tarefa">
-            <Trash size={18} />
-          </button>
-        </li>
-        <li>
-          <input id="item-4" type="checkbox" name="item-4" value="item-4" />
-          <label htmlFor="item-4">
-            Integer urna interdum massa libero auctor neque turpis turpis
-            semper. Duis vel sed fames integer.
-          </label>
-          <button onClick={() => {}} title="Deletar tarefa">
-            <Trash size={18} />
-          </button>
-        </li>
-        <li>
-          <input id="item-5" type="checkbox" name="item-5" value="item-5" />
-          <label htmlFor="item-5">
-            Integer urna interdum massa libero auctor neque turpis turpis
-            semper. Duis vel sed fames integer.
-          </label>
-          <button onClick={() => {}} title="Deletar tarefa">
-            <Trash size={18} />
-          </button>
-        </li>
-      </ul>
+      {tasks.length > 0 && (
+        <ul>
+          {tasks.map((item) => {
+            return (
+              <li
+                key={`task-${item.id}`}
+                onClick={() => {
+                  handleCheckedTask(item.id);
+                }}
+              >
+                <label
+                  className={`${item.isChecked ? Styled.checkboxChecked : ''}`}
+                >
+                  {item.label}
+                </label>
+                <input
+                  id={`task-${item.id}`}
+                  type="checkbox"
+                  name={`task-${item.id}`}
+                  value={`${item.label}`}
+                  defaultChecked={item.isChecked}
+                />
+                <button
+                  onClick={(e) => handleDeleteTask(e, item.id)}
+                  title="Deletar tarefa"
+                >
+                  <Trash size={18} />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
